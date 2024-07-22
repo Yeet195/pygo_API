64dcbe256bfb0bbeefb693ce129b9d28c18cb5db
Yeet195 <nils.kalinke11@gmail.com>
2024-07-22 13:02:02 +0200

    fixed setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='pygo',
+    name='pygo_YGOProDeck_API_module',
     version='1.0',
     packages=find_packages(),
     install_requires=[
